deaa5849d0282cf488dfdaae8b2ad30ee6cb677f
Shun Kong Cheung <shcheung@microsoft.com>
2022-11-15 23:51:40 -0800

    feat(getPieceScore): rearrange scores

--- src/chess/getPieceScore.ts
+++ src/chess/getPieceScore.ts
@@ -2,19 +2,19 @@ import { Piece } from "../types";
 
 const getPieceScore = (piecePrefix: string) => {
   const piecePrefixUpper = piecePrefix.toUpperCase() as Piece;
-  const pieceScore = [
-    Piece.EMPTY,
-    Piece.SOLDIER,
-    Piece.JUMBO,
-    Piece.KNIGHT,
-    Piece.CANNON,
-    Piece.HORSE,
-    Piece.CASTLE,
-    Piece.GENERAL,
-  ];
+  const pieceScore = {
+    [Piece.EMPTY]: 0,
+    [Piece.SOLDIER]: 1,
+    [Piece.JUMBO]: 3,
+    [Piece.KNIGHT]: 4,
+    [Piece.CANNON]: 7,
+    [Piece.HORSE]: 6,
+    [Piece.CASTLE]: 9,
+    [Piece.GENERAL]: 10,
+  };
 
-  const pieceIndex = pieceScore.indexOf(piecePrefixUpper);
-  return piecePrefixUpper === piecePrefix ? pieceIndex : -pieceIndex;
+  const value = pieceScore[piecePrefixUpper];
+  return piecePrefixUpper === piecePrefix ? value : -value;
 };
 
 export default getPieceScore;
