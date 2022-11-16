65fa3d0214261476cb943b1c34e0c9195869098b
Shun Kong Cheung <shcheung@microsoft.com>
2022-11-15 22:29:48 -0800

    feat(Playground): components

--- src/containers/Playground.tsx
+++ src/containers/Playground.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 import { Board, Side } from "../types";
 import { NetworkNode } from "../utils/NetworkNode";
-import ChessBoard from "../components/ChessBoard";
+import { Card, ChessBoard, Container } from "../components";
 
 import { Payload, Result } from "../pages/api/simulate";
 import { getBoardWinnerAndScore, getHashFromBoard } from "../chess";
@@ -20,53 +20,9 @@ interface IState {
   side: Side;
 }
 
-const Container = styled.div`
-  max-width: 1080px;
-  margin-left: auto;
-  margin-right: auto;
-  display: grid;
-  grid-template-columns: 3fr 2fr;
-  gap: 1.2rem;
-  flex-wrap: wrap;
-`;
-
-const MoveList = styled.ul`
-  list-style: none;
-  margin: 0;
-  padding: 0;
-  border: 1px solid #ccc;
-  border-radius: 0.2rem;
-  height: 100%;
-  overflow: auto;
-`;
-
-const MoveItem = styled.li`
-  padding: 0.7rem 1.5rem;
-  border-bottom: 1px solid #ccc;
-  :last-of-type {
-    border-bottom: 0px;
-  }
-`;
-
-const Card = styled.div`
-  box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.2), 0 4px 20px 0 rgba(0, 0, 0, 0.19);
-  list-style: none;
-  padding: 0;
-  margin: 0;
-`;
-
-const Desc = styled.li`
-  display: flex;
-  justify-content: space-between;
-  padding: 5px;
-  border-bottom: 1px solid #eee;
-`;
-
-const Title = styled.div`
-  font-weight: bold;
-`;
-
-const Value = styled.div``;
+const Main = styled.div `
+width: 500px;
+`
 
 const copyHash = (board: Board) => {
   const text = getHashFromBoard(board);
@@ -100,24 +56,10 @@ const Playground = ({ board, side, runTimes }: IProps) => {
 
   const handleRun = useCallback(async () => {
     const board = state.records[0][0].board;
-    const [winner, score] = getBoardWinnerAndScore(board);
     const payload: Payload = {
       levelZeroSide: state.side,
       runTimes,
-      openSet: [
-        {
-          board,
-          index: 0,
-          children: [],
-          relatives: [],
-          score,
-          winner,
-          level: 0,
-          priority: 0,
-          isOpenForCalculation: true,
-          isTerminated: false,
-        },
-      ],
+      board,
     };
 
     const response = await fetch("/api/simulate", {
@@ -142,31 +84,22 @@ const Playground = ({ board, side, runTimes }: IProps) => {
 
   return (
     <Container>
-      <Card>
-        <div onClick={() => copyHash(state.records[0][0].board)}>
-          <ChessBoard board={state.records[0][0].board} />
-        </div>
-        <Desc>
-          <Title>Run</Title>
-          <Value>{runTimes}</Value>
-        </Desc>
-        <Desc>
-          <Title>Side</Title>
-          <Value>{state.side}</Value>
-        </Desc>
-        <Desc>
-          <Title>Time taken</Title>
-          <Value>{state.timeTaken}ms</Value>
-        </Desc>
-        <Desc>
-          <Title>
-            <button onClick={handleBack}>back</button>
-          </Title>
-          <Value>
-            <button onClick={handleRun}>next</button>
-          </Value>
-        </Desc>
-      </Card>
+      <Main>
+        <Card
+          descriptions={[
+            { title: "Run", value: runTimes} ,
+            { title: "Side", value: state.side} ,
+            { title: "Time taken", value: `${state.timeTaken}ms`} ,
+            { title: <button onClick={handleBack}>back</button>,
+              value: <button onClick={handleRun}>next</button>}
+          ]}
+        >
+          <div onClick={() => copyHash(state.records[0][0].board)}>
+            <ChessBoard board={state.records[0][0].board} />
+          </div>
+
+        </Card>
+      </Main>
     </Container>
   );
 };
