6badc37ebca09aef30cd09ee87646f16b851091f
Shun Kong Cheung <shcheung@microsoft.com>
2022-11-16 17:06:30 -0800

    feat(Logger): format

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,6 +1,6 @@
 export const getLogger = (name: string) => {
   return (str: string) => {
     const time = new Date().toLocaleTimeString();
-    console.log(`${time}[${name}]: ${str}`);
+    console.log(`${time} [${name}]: ${str}`);
   };
 };
