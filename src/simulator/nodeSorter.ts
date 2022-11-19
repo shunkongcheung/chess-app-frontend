96cfd524c1206a4ba2470edcfa2305007116f492
Shun Kong Cheung <shcheung@microsoft.com>
2022-11-19 11:16:50 -0800

    feat(nodeSorter): no change

--- src/simulator/nodeSorter.ts
+++ src/simulator/nodeSorter.ts
@@ -1,6 +1,7 @@
 interface SortNode {
   level: number;
   priority: number;
+  score: number;
 }
 
 const nodeSorter = (left: SortNode, right: SortNode) => {
@@ -9,6 +10,12 @@ const nodeSorter = (left: SortNode, right: SortNode) => {
 
   if (left.level < right.level) return -1;
   if (left.level > right.level) return 1;
+
+  // this code seems to have done nothing
+  // const leftAbsScore = Math.abs(left.score);
+  // const rightAbsScore = Math.abs(right.score);
+  // if (leftAbsScore > rightAbsScore) return -1;
+  // if (leftAbsScore < rightAbsScore) return 1;
 
   return 0;
 };
